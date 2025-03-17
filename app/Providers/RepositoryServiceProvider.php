8b0338f08a965c2e78119b8eb9adb8d70f826065
Alisher <myemail9445@inbox.ru>
2025-03-17 16:45:49 +0500

    service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\BaseRepository;
+use App\Repositories\Contracts\IEventImageRepository;
+use App\Repositories\Contracts\IEventParticipantRepository;
+use App\Repositories\Contracts\IEventRepository;
+use App\Repositories\Contracts\IRepository;
+use App\Repositories\Contracts\IUserFineRepository;
+use App\Repositories\Contracts\IUserRepository;
+use App\Repositories\EventImageRepository;
+use App\Repositories\EventParticipantRepository;
+use App\Repositories\EventRepository;
+use App\Repositories\UserFineRepository;
+use App\Repositories\UserRepository;
+use App\Strategies\FineCalculation\IFineCalculationStrategy;
+use App\Strategies\Notification\INotificationStrategy;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(IRepository::class, BaseRepository::class);
+        $this->app->bind(IUserRepository::class, UserRepository::class);
+        $this->app->bind(IEventRepository::class, EventRepository::class);
+        $this->app->bind(IEventParticipantRepository::class, EventParticipantRepository::class);
+        $this->app->bind(IEventImageRepository::class, EventImageRepository::class);
+        $this->app->bind(IUserFineRepository::class, UserFineRepository::class);
+
+        $this->app->bind(IFineCalculationStrategy::class, function ($app) {
+            return [
+                $app->make('App\Strategies\FineCalculation\DefaultFineStrategy'),
+                $app->make('App\Strategies\FineCalculation\ProgressiveFineStrategy')
+            ];
+        });
+
+        $this->app->bind(INotificationStrategy::class, function ($app) {
+            return [
+                $app->make('App\Strategies\Notification\PushNotificationStrategy'),
+                $app->make('App\Strategies\Notification\InAppNotificationStrategy'),
+                $app->make('App\Strategies\Notification\EmailNotificationStrategy')
+            ];
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
